a9b2f84d836cb3a5397b398d1b44c51ff919c1d7
arunsammit <arunsammit@gmail.com>
2022-02-07 15:48:37 +0530

    added utility function for finding the bugs in the dataset

--- graphdataset.py
+++ graphdataset.py
@@ -118,6 +118,32 @@ def write_sizes(root, size_file_dir):
             data = torch.load(os.path.join(root, file), map_location=torch.device('cpu'))
             sizes_file.write(f"{file}: {len(data)}\n")
     return
+def debug_dataset(root, bugs_output_file_dir):
+    files = sorted(
+        list(filter(lambda f: os.path.isfile(os.path.join(root,f)), os.listdir(root))), 
+        key=lambda f: int(re.split('_|[.]',f)[-2])
+    )
+    with open(os.path.join(bugs_output_file_dir, 'data_bugs.txt'), 'w') as data_bugs_file:
+        problematic_files = set()
+        for file in files:
+            datalist = torch.load(os.path.join(root, file), map_location=torch.device('cpu'))
+            for i, data in enumerate(datalist):
+                with torch.no_grad():
+                    # print(data)
+                    try:
+                        max_val_edge = data.edge_index.max().item()
+                    except RuntimeError as e:
+                        data_bugs_file.write(f"exception {e}:\t")
+                        data_bugs_file.write(f"{file} has graph {i} with {data.num_nodes} nodes, edge_index.size(): {data.edge_index.size()}, edge_attr.size(): {data.edge_attr.size()}\n") 
+                        problematic_files.add(file)
+                    if data.num_nodes <= max_val_edge:
+                        data_bugs_file.write(f"{file}: graph {i} {data.num_nodes} nodes, {max_val_edge} is present in the edge list\n")
+                        problematic_files.add(file)
+            # sizes_file.write(f"{file}: {len(datalist)}\n")
+        data_bugs_file.write(f"{len(problematic_files)} files have problems\n")
+        for file in problematic_files:
+            data_bugs_file.write(f"{file}\n")
+    return
 #%%
 def cleanup():
     import subprocess
@@ -125,6 +151,6 @@ def cleanup():
 
 if __name__ == '__main__':
     cleanup()
-    main()
-    # write_sizes('data_tgff/multiple/train/raw','data_tgff/multiple/train')
+    # main()
+    debug_dataset('data_tgff/multiple/train/raw','data_tgff/multiple/train')
    
