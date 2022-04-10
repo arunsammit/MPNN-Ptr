tried to fix a wierd bug where the backprop was failing by using .clone() method

models/transformers.py
@@ -160,15 +160,15 @@ class TransformerPointerNet(nn.Module):
             hidden_dim = node_embeddings.size(-1)
             predicted_mappings[:, :, t] = selected_indices
             node_embeds_batch_first = node_embeddings.transpose(0, 1)
-            prev_indices = predicted_mappings[:,:, t-1]
-            prev_decoded_embeddings = node_embeds_batch_first.gather(1, prev_indices.unsqueeze(-1).expand(-1,-1, hidden_dim))\
-                .view(batch_size * num_samples, -1)
-            first_indices = predicted_mappings[:,:,0]
-            first_decoded_embeddings = node_embeds_batch_first.gather(1, first_indices.unsqueeze(-1).expand(-1,-1,hidden_dim))\
+            first_indices = predicted_mappings[:,:,0].unsqueeze(-1).clone()
+            first_indices = first_indices.masked_fill(first_indices == -1, 0)
+            first_decoded_embeddings = node_embeds_batch_first.gather(1, first_indices.expand(-1,-1,hidden_dim))\
                 .view(batch_size * num_samples, -1)
             # creating the indices for gathering log_probs corresponding to choosen indices
             gather_indices = selected_indices.unsqueeze(-1).clone()
-            gather_indices[gather_indices == -1] = 0
+            gather_indices = gather_indices.masked_fill(gather_indices == -1, 0)
+            prev_decoded_embeddings = node_embeds_batch_first.gather(1, gather_indices.expand(-1,-1, hidden_dim))\
+                .view(batch_size * num_samples, -1)
             curr_log_probs = log_probs.gather(-1, gather_indices).squeeze(-1) * mask_multiple_samples[:,:, t]
             log_probs_sum += curr_log_probs
             if self.decoding_type == 'sampling-w/o-replacement':

train_multiple_graph.py
@@ -68,15 +68,15 @@ datetime_suffix = datetime.now().strftime('%m-%d_%H-%M')
 save_folder.mkdir(parents=True, exist_ok=True)
 # %%
 torch.autograd.set_detect_anomaly(True)
-avg_valid_comm_cost = validate_dataloader(
-    mpnn_ptr, tqdm(dev_dataloader, leave=False), distance_matrix_dict, beam_width) / len(dev_dataloader.dataset)
-loss_list_dev.append(avg_valid_comm_cost)
-print_str = f'Epoch: 0/{num_epochs} Dev Comm cost: {avg_valid_comm_cost}'
-print(print_str)
+# avg_valid_comm_cost = validate_dataloader(
+#     mpnn_ptr, tqdm(dev_dataloader, leave=False), distance_matrix_dict, beam_width) / len(dev_dataloader.dataset)
+# loss_list_dev.append(avg_valid_comm_cost)
+# print_str = f'Epoch: 0/{num_epochs} Dev Comm cost: {avg_valid_comm_cost}'
+# print(print_str)
 logs_save_folder = save_folder / "logs"
 logs_save_folder.mkdir(parents=True, exist_ok = True)
 f = open(logs_save_folder / f'{datetime_suffix}_train_loss.txt', 'a')
-f.write(f"{print_str}\n")
+# f.write(f"{print_str}\n")
 model_save_folder = save_folder / "models_data"
 model_save_folder.mkdir(parents=True, exist_ok=True)
 per_epoch_save_folder = model_save_folder / f'per_epoch'